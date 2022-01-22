ca27b796fca6eb2c7b72c67b8c9ee51fd29c1043
Davis <dsauer@uw.edu>
2022-01-22 13:20:49 -0800

    tf?

---
+++ I2C bus PWM Controller.cydsn/codegentemp/I2C bus PWM Controller_u.sdc
@@ -0,0 +1,3 @@
+# Component constraints for C:\Users\davis\Documents\PSoC Creator\PCA9685_Driver\I2C bus PWM Controller.cydsn\TopDesign\TopDesign.cysch
+# Project: C:\Users\davis\Documents\PSoC Creator\PCA9685_Driver\I2C bus PWM Controller.cydsn\I2C bus PWM Controller.cyprj
+# Date: Wed, 19 Jan 2022 02:24:07 GMT
---
+++ I2C bus PWM Controller.cydsn/main.c
@@ -0,0 +1,27 @@
+#include "project.h"
+#include "PCA9685.h"
+
+int main(void)
+{
+    CyGlobalIntEnable; /* Enable global interrupts. */
+    
+    pca_init();
+    
+    for(;;)
+    {
+        //sets individual on and off time
+        setPWMFromBytes(PIN0, 0x00, 0x00, 0xE0, 0x02);
+        CyDelay(1000);
+        
+        for(int i =0; i<100; i++){
+            //sets pwm signal based on duty cycle (0-100)
+            setPWMFromDutyCycle(PIN0, i);
+            CyDelay(1);
+        }
+        for(int i =99; i>=0; i--){
+            setPWMFromDutyCycle(PIN0, i);
+            CyDelay(1);
+        }
+        
+    }
+}
